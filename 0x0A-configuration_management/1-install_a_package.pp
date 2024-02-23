a3b879ba7069abf6642722cc3a6b53460f1adb2d
jomojay <djdrjay001@gmail.com>
2024-02-23 23:19:08 +0100

    properly indented task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# puppet declarative to install flask using pip3
+#puppet declarative to install flask using pip3
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
