2535319bc03569f4a8543bff7cd0992fbd761863
jomojay <djdrjay001@gmail.com>
2024-05-07 18:59:28 +0100

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,11 +3,9 @@
 from requests import get
 
 
-url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-
-
 def number_of_subscribers(subreddit):
     '''get subscribers total number'''
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     header = {'user-agent': 'jomojay-app0'}
     res = get(url, headers=header)
     sub_total = res.json()
